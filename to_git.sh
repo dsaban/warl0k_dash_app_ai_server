fdac35404cd633f69385c69070c0ee909164f064
Danny Saban <danny@illumex.ai>
2025-06-29 12:33:44 +0300

    update requirements.txt

--- to_git.sh
+++ to_git.sh
@@ -10,7 +10,7 @@
 
 # Initialize a new Git repository
 git add .
-git commit -m "update code for warl0k_controlroom_combined.py"
+git commit -m "update requirements.txt"
 git remote add origin git@github.com:dsaban/warl0k_dash_app_ai_server.git
 git branch -M main
 git push -u origin main
